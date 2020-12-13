1f9282e22fad4ed068c31fef4f1d819dc19384fb
Larvata Lin <alzzlnewtype@gmail.com>
2020-12-13 22:28:26 +0800

    bug fix

--- ss-run.sh
+++ ss-run.sh
@@ -2,8 +2,8 @@
 # set -x
 
 input="ss.lst"
-# idx=0
-# lport=9000
+idx=0
+lport=9000
 logfile="ss.log"
 logpath=/var/log/$logfile
 
@@ -14,28 +14,16 @@ fi
 
 while IFS= read -r line
 do
-  # if [ $idx == 0 ]; then
-  #   port=$line
-  # fi
-  # if [ $idx == 1 ]; then
-  #   method=$line
-  # fi
-  # if [ $idx == 2 ]; then
-  #   key=$line
-  # fi
+  lport=$((lport+1))
 
-  if [ $idx -gt 2 ] && [ ! -z "$line" ]; then
-    # cmd="ss-local -s $line -p $port -m $method -k $key -l $lport -b 0.0.0.0 --plugin obfs-local --plugin-opts 'obfs=http;obfs-host=a9fa6129375.microsoft.com'"
-    # lport=$((lport+1))
-
-    if [ "$1" == "-v" ]; then
-      cmd="$line -v >> $logpath &"
-    else
-      cmd="$line -f $idx.pid"
-    fi
-    # echo $cmd
-    eval "$cmd"
+  if [ "$1" == "-v" ]; then
+    cmd="$line -v >> $logpath &"
+  else
+    cmd="$line -f $idx.pid"
   fi
+
+  # echo $cmd
+  eval "$cmd"
 
   idx=$((idx+1))
 done < $input
